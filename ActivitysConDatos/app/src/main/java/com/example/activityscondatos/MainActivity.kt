4cff83c7e4eceb19c3fb649a3ea45797584acd2e
AperezC <albertopecal@gmail.com>
2023-10-09 18:08:51 +0200

    Comit Navega Entre Ventanas

--- ActivitysConDatos/app/src/main/java/com/example/activityscondatos/MainActivity.kt
+++ ActivitysConDatos/app/src/main/java/com/example/activityscondatos/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.activityscondatos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
+import com.example.activityscondatos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var binding: ActivityMainBinding
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.boton.setOnClickListener{
+            IrVentana2()
+        }
+    }
+
+    private fun IrVentana2() {
+        var miIntent: Intent = Intent(this, Ventana2::class.java)
+        startActivity(miIntent)
     }
 }
